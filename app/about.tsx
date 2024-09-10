64d42c41e2deef1ff99b9d88b68b647e7f847e59
Rhuan Gustavo Miranda <153655600+miragus@users.noreply.github.com>
2024-09-10 17:01:04 -0300

    10/09

--- app/about.tsx
+++ app/about.tsx
@@ -3,6 +3,8 @@ import { LinearGradient } from 'expo-linear-gradient';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { cloneElement } from 'react';
+
 
 
 export default function App() {
@@ -54,13 +56,125 @@ export default function App() {
 
             <View style={styles.container2}>
 
+                <View style={styles.search}>
+                    <TextInput style={styles.talkTo}
+                        placeholder='        Fale com o'
+                    />
+
+                    <Text style={styles.inputText}>Gusta</Text>
+
+                    <Image style={styles.lupaIcon} source={require('../assets/lupa.png')} />
+
+
+                    <View style={styles.iconMic}>
+
+                        <LinearGradient
+                            colors={['#C51F69', 'transparent']}
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                                borderRadius: 8,
+                                transform: [{ scaleY: -1 }]
+                            }}
+                        />
+
+                        <Image style={{
+                            bottom: 31,
+                            left: 16,
+                            width: 21,
+                            height: 21
+                        }} source={require('../assets/microphone.png')} />
+
+                    </View>
+                </View>
+
+                <View style={styles.MbOf}>
+
+                    <View style={styles.myBank}>
+
+                        <Image style={{
+                            width: 22,
+                            height: 22,
+                            zIndex: 1
+                        }}
+                            source={require('../assets/bank.png')}
+                        />
+
+                        <LinearGradient
+                            colors={['#C51F69', 'transparent']}
+                            style={styles.gradientMbOf}
+                        />
+
+                    </View>
+
+                    <Text style={styles.MbOfTxt}>Meus Bancos</Text>
+
+                    <View style={styles.openFinance}>
+
+                        <Image style={{
+                            width: 20,
+                            height: 21,
+                            zIndex: 1
+                        }}
+                            source={{ uri: 'https://dashboard.openfinancebrasil.org.br/icon.png' }}
+                        />
+
+                        <LinearGradient
+                            colors={['#C51F69', 'transparent']}
+                            style={styles.gradientMbOf}
+                        />
+
+                    </View>
+
+                    <Text style={styles.MbOfTxt}>Open Finance</Text>
+                </View>
+
+
+                <View style={{
+                    borderWidth: 1,
+                    width: '100%',
+                    height: 'auto',
+                    gap: 15,
+                    justifyContent:'center',
+                    flexDirection:'column'
+                }}>
+
+                <View style={{
+                    backgroundColor: '#FFFFFF',
+                    height: 50,
+                    width: 50
+                }}>
+
+                </View>
+
+                <View style={{
+                    backgroundColor: '#FFFFFF',
+                    height: 50,
+                    width: 50
+                }}>
+
+                </View>
+
+                <View style={{
+                    backgroundColor: '#FFFFFF',
+                    height: 50,
+                    width: 50
+                }}>
+
+                </View>
+
             </View>
 
 
 
 
+        </View>
 
-        </SafeAreaView>
+
+
+
+
+        </SafeAreaView >
     );
 }
 
@@ -145,8 +259,79 @@ const styles = StyleSheet.create({
         marginTop: 50,
         backgroundColor: '#EFF1F5',
         width: '100%',
+        height: '100%',
+        alignItems: 'center'
+    },
+    talkTo: {
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center',
+        borderRadius: 8,
+        width: '100%',
         height: '100%'
     },
+    search: {
+        width: '85%',
+        height: 40,
+        flexDirection: 'row',
+        bottom: 15,
+    },
+    inputText: {
+        zIndex: 1,
+        right: 230,
+        top: 9,
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#D31C65'
+    },
+    lupaIcon: {
+        zIndex: 1,
+        right: 120,
+        top: 9,
+        width: 22,
+        height: 22
+    },
+    iconMic: {
+        width: 46,
+        height: '100%',
+        right: 110,
+        borderRadius: 8,
+        backgroundColor: '#F31D5A'
+    },
+    MbOf: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '90%',
+        justifyContent: 'space-between'
+    },
+    myBank: {
+        zIndex: 3,
+        width: 45,
+        height: 40,
+        borderRadius: 10,
+        backgroundColor: '#F31D5A',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    openFinance: {
+        zIndex: 3,
+        width: 45,
+        height: 40,
+        borderRadius: 10,
+        backgroundColor: '#F31D5A',
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
+    gradientMbOf: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        borderRadius: 8,
+        transform: [{ scaleY: -1 }]
+    },
+    MbOfTxt: {
+        right: 16
+    }
 
 
 
